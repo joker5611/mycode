ebd012f92ccd7d4fd62b0b12b2ac6f3411b54c1b
Josh Justus <joker5611@gmail.com>
2021-09-13 16:18:30 +0000

    Lists and Looping

---
+++ mix-list/mix_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#print("The first item in the list (IP): " + my_list[0] )
+
+#print("The second item in the list (port): " + str(my_list[1]) )
+
+#print("The last item in the list (state): " + my_list[2] )
+
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("the ip addresses are " + new_list[3] +"and "+ new_list[4] )
+
