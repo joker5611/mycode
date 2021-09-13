05ec55e4fae3faa4dc5d3c6b539ae7e4339e1328
Josh Justus <joker5611@gmail.com>
2021-09-13 18:41:12 +0000

    json to pyton

--- dict01/dict01.py
+++ dict01/dict01.py
@@ -18,3 +18,25 @@ print( switch.get('lynx', "THE KEY IS IN ANOTHER CASTLE!") )
 
 print( "Third test - .get()" )
 print( switch.get('version') )
+
+print( "Fourth test - .keys()" )
+print( switch.keys() )
+
+print( "Fifth test - .values()" )
+print( switch.values() )
+print( "Sixth test - .pop()" )
+switch.pop('version') # removes this key (and value) pair
+print( switch.keys() )   # notice the value of version is gone
+print( switch.values() ) # notice the value 1.2
+
+print( "Seventh test - ADD a new value" )
+switch['adminlogin'] = 'karl08'
+print( switch.keys() )
+print( switch.values() )
+
+print( "Eighth test - ADD a new value" )
+switch['password'] = 'qwerty'
+print( switch.keys() )
+print( switch.values() )
+
+
