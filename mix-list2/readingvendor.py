49f6302cf772fd5ee1adfaeeb058d2487d83d585
Josh Justus <joker5611@gmail.com>
2021-09-13 17:28:02 +0000

    copying files and folders

--- mix-list2/readingvendor.py
+++ mix-list2/readingvendor.py
@@ -6,7 +6,7 @@ def main():
     # open the CSV data set
     with open("vendor.csv", "r") as vendorfile:
         vendata = csv.reader(vendorfile, delimiter=",")
-        # The CSV data is now nicely prepared.
+       # The CSV data is now nicely prepared.
         # Each new row is a Python list.
         # We can reference the items we want to print
         # as we loop over the data.
