c776d86d209d17d7d4dc4eeb77265cc78dd078f5
Josh Justus <joker5611@gmail.com>
2021-09-15 14:22:12 +0000

    update nasa 02

--- nasa02/nasa02updated.py
+++ nasa02/nasa02updated.py
@@ -7,13 +7,16 @@ def main():
     neourl = 'https://api.nasa.gov/neo/rest/v1/feed?' # API URL
     startdate = 'start_date=2018-06-01'  ## start date for data
     enddate = '&end_date=END_DATE' ## create a mechanism to utilize enddate
-    mykey = '&api_key=DEMO_KEY' ## replace this with our API key
+    mykey = '&api_key=GUi3WOIHrksyfld4sz503GhgZlL0l9qymu4sMKTm' ## replace this with our API key
 
     neourl = neourl + startdate + mykey
 
     neojson = (requests.get(neourl)).json()
 
-    print(neojson)
+    for date in neojson['near_earth_objects'].keys():
+        for ast_dict in neojson['near_earth_objects'][date]:
+            print(ast_dict["name"])
+    
 
 ## call main
 main()
