2ccd74c9734c243c148310042912e17d1ee6f919
Josh Justus <joker5611@gmail.com>
2021-09-15 12:29:45 +0000

    for loop , list of dicts

---
+++ iss/ride_iss.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Alta3 || Tracking ISS"""
+
+import urllib.request
+import json
+
+## Define URL
+MAJORTOM = 'http://api.open-notify.org/astros.json'
+
+def main():
+        
+    ## Call the webservice
+    groundctrl = urllib.request.urlopen(MAJORTOM)
+    
+    ## put fileobject into helmet
+    helmet = groundctrl.read()
+    
+    ## decode JSON to Python data structure
+    helmetson = json.loads(helmet.decode('utf-8'))
+    
+    print(f'People in space: {helmetson["number"]}')
+    for x in helmetson["people"]:
+        print(f"{x['name']} is on the {x['craft']}.")
+     
+    
+main()
+
