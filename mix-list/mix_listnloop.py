4f3e886dbbd30965f642dabea9439dd337185925
Josh Justus <joker5611@gmail.com>
2021-09-18 18:20:10 +0000

    Test for Joe and Herb

--- mix-list/mix_listnloop.py
+++ mix-list/mix_listnloop.py
@@ -5,8 +5,7 @@ def main():
     # the switch IP addresses
     networklists = [['ios', '10.0.2.1'], ['ios', '10.0.55.1'], ['ios-xr', '10.0.3.1'], ['junos', '10.0.10.1'], ['eos', '10.0.14.1']]
 
-    for driveandip in networklists:
-        print('SSH to ' + driveandip[1] + ' using driver ' + driveandip[0])
+    for x in networklists:
+        print('SSH to ' + x[1] + ' using driver ' + x[0])
 
 main()
-
